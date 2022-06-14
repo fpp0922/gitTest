4a2ee5709f1429143fb104551fefd3ec98b0f0ad
dfy <fpp0922@163.com>
2022-06-14 18:50:50 +0800

    first hot commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -1,6 +1,8 @@
 public class gitTest {
     public static void main(String[] args){
         System.out.println("git1");
+        System.out.println("git-hot1");
+        System.out.println("git-hot2");
 
     }
 }
