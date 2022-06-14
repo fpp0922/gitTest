66674904cdffeddeb7d43fe9e92671e419514eb1
dfy <fpp0922@163.com>
2022-06-14 18:52:53 +0800

    second hot commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -3,6 +3,8 @@ public class gitTest {
         System.out.println("git1");
         System.out.println("git-hot1");
         System.out.println("git-hot2");
+        System.out.println("git-hot5");
+        System.out.println("git-hot6");
 
     }
 }
